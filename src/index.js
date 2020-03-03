954830d6f5b0aa1bf3ca80a6c41a2d5960f5e470
James White <james.white@my.wheaton.edu>
2020-03-03 15:41:22 -0500

    gitignore

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+
+// Doing this tutorial
+// https://www.freecodecamp.org/news/fullstack-react-blog-app-with-express-and-psql/
+
+// Making the starter app
+// https://www.freecodecamp.org/news/build-a-react-hooks-front-end-app-with-routing-and-authentication/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
